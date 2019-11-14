f9bc6e68484459aae656a8c171ddeeaf3e039012
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-11-14 11:27:17 -0600

    npm exercises

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import $ from 'jquery';
+
+const $ = require('jquery');
+const sayHello = () => console.log('hello there');
+
+sayHello();
+
+$('body').css('background-color', '#0f6367').html('<h1>Hello nmp!</h1>').css('color', "white");
+
+
