442f6140c62fe196cc8c3d4e065f63f626a0b1f1
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-11-14 11:37:58 -0600

    npm exercise

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ const sayHello = () => console.log('Hello there!');
 
 sayHello();
 
-$('body').css('background-color', '#0f6367').html('<h1>Hello from nmp!</h1>').css('color', "white");
+$('body').css('background-color', '#FF4571').html('<h1>Hello from nmp!</h1>').css('color', "white");
 
 
